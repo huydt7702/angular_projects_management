3e1826ded00a96225655db0df346425832c5232b
HuyNguyen <developer.vanhuy@gmail.com>
2023-06-01 21:04:36 +0700

    Handle delete project frontend

--- frontend/src/app/components/project-list/project-list.component.ts
+++ frontend/src/app/components/project-list/project-list.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, TemplateRef } from '@angular/core';
+import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 import { Project } from 'src/app/models/project';
 import { DataService } from 'src/app/services/data.service';
 import { RestApiService } from 'src/app/services/rest-api.service';
@@ -12,8 +13,45 @@ export class ProjectListComponent implements OnInit {
   projects!: Project[];
   btnDisabled = false;
   url = 'http://localhost:4040/v1/api/projects';
+  deletedId!: string;
+  confirmMessage = '';
 
-  constructor(private rest: RestApiService, private data: DataService) {}
+  constructor(
+    private rest: RestApiService,
+    private data: DataService,
+    private modalService: NgbModal
+  ) {}
+
+  confirmDeleteProject(
+    confirmDialog: TemplateRef<any>,
+    id: string,
+    name: string
+  ) {
+    this.confirmMessage = `Do you want to delete the project ${name}`;
+    this.deletedId = id;
+    this.modalService
+      .open(confirmDialog, { ariaDescribedBy: 'modal-basic-title' })
+      .result.then(
+        (result) => {
+          this.deletedId = '';
+        },
+        (err) => {}
+      );
+  }
+
+  deleteProject() {
+    if (this.deletedId !== '') {
+      this.rest
+        .delete(this.url, this.deletedId)
+        .then((data) => {
+          this.modalService.dismissAll();
+          this.ngOnInit();
+        })
+        .catch((error) => {
+          this.data.error(error['message']);
+        });
+    }
+  }
 
   ngOnInit() {
     this.btnDisabled = true;
