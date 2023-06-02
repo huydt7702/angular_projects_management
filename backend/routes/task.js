700142c8e0c006624d8db72e3f51ddfc9d256b5c
HuyNguyen <developer.vanhuy@gmail.com>
2023-06-02 10:10:24 +0700

    Create router put and delete task backend

--- backend/routes/task.js
+++ backend/routes/task.js
@@ -61,4 +61,64 @@ router.get("/:id", async (req, res) => {
     });
 });
 
+router.put("/:id", async (req, res, next) => {
+    const taskExist = await Task.findById(req.params.id);
+
+    if (!taskExist) {
+        return res.status(404).json({
+            success: false,
+            message: "The task Not Found",
+        });
+    }
+
+    let task = {
+        taskName: req.body.taskName,
+        projectName: req.body.projectName,
+        description: req.body.description,
+        assignedTo: req.body.assignedTo,
+        priority: req.body.priority,
+        status: req.body.status,
+    };
+
+    try {
+        task = await Task.findByIdAndUpdate(req.params.id, task, { new: true });
+
+        res.json({
+            success: true,
+            data: task,
+        });
+    } catch (error) {
+        console.log(error);
+        return res.status(500).json({
+            success: false,
+            message: "The task cannot be updated",
+            error: error,
+        });
+    }
+});
+
+router.delete("/:id", async (req, res) => {
+    Task.findByIdAndRemove(req.params.id)
+        .then((task) => {
+            if (task) {
+                return res.status(200).json({
+                    success: true,
+                    message: "The task is deleted",
+                });
+            }
+
+            return res.status(404).json({
+                success: false,
+                message: "The task with the given ID was not found",
+            });
+        })
+        .catch((error) => {
+            return res.status(500).json({
+                success: false,
+                message: "The task cannot be deleted",
+                error: error,
+            });
+        });
+});
+
 module.exports = router;
