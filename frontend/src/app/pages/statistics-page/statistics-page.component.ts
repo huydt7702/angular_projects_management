02195bcd1251fd2e913422f70eab33d05dbf2bad
HuyNguyen <developer.vanhuy@gmail.com>
2023-06-16 15:45:59 +0700

    Complete statistics page

--- frontend/src/app/pages/statistics-page/statistics-page.component.ts
+++ frontend/src/app/pages/statistics-page/statistics-page.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ToastrService } from 'ngx-toastr';
 import { Project } from 'src/app/models/project';
+import { Task } from 'src/app/models/task';
 import { DataService } from 'src/app/services/data.service';
 import { RestApiService } from 'src/app/services/rest-api.service';
 
@@ -11,8 +12,12 @@ import { RestApiService } from 'src/app/services/rest-api.service';
 })
 export class StatisticsPageComponent implements OnInit {
   projects!: Project[];
+  tasks!: Task[];
   projectId!: string;
   project: Project;
+  totalCostOfAllProjects: number = 0;
+  employeeCount!: number;
+  tasksInProgress!: number;
   url = 'http://localhost:4040/v1/api/projects';
 
   constructor(
@@ -28,6 +33,34 @@ export class StatisticsPageComponent implements OnInit {
       .get(this.url)
       .then((data) => {
         this.projects = (data as { data: Project[] }).data;
+        this.totalCostOfAllProjects = this.projects.reduce(
+          (total, currProject) => total + currProject.budget,
+          0
+        );
+      })
+      .catch((error) => {
+        this.data.error(error['message']);
+        this.toasrt.error(error['message'], 'Error!');
+      });
+
+    this.rest
+      .get('http://localhost:4040/v1/api/accounts/get/count')
+      .then((data) => {
+        this.employeeCount = (data as { employeeCount: number }).employeeCount;
+      })
+      .catch((error) => {
+        this.data.error(error['message']);
+        this.toasrt.error(error['message'], 'Error!');
+      });
+
+    this.rest
+      .get('http://localhost:4040/v1/api/tasks')
+      .then((data) => {
+        const tasks = (data as { data: Task[] }).data;
+        const listTasksInProgress = tasks.filter(
+          (task) => task.status === 'Developing'
+        );
+        this.tasksInProgress = listTasksInProgress.length;
       })
       .catch((error) => {
         this.data.error(error['message']);
