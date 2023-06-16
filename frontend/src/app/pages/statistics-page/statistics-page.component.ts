ea345801cb9ef897ebd0bdaaf15e580a9beb5642
HuyNguyen <developer.vanhuy@gmail.com>
2023-06-16 10:27:14 +0700

    Build statistical UI and handle some logic

--- frontend/src/app/pages/statistics-page/statistics-page.component.ts
+++ frontend/src/app/pages/statistics-page/statistics-page.component.ts
@@ -1,10 +1,49 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ToastrService } from 'ngx-toastr';
+import { Project } from 'src/app/models/project';
+import { DataService } from 'src/app/services/data.service';
+import { RestApiService } from 'src/app/services/rest-api.service';
 
 @Component({
   selector: 'app-statistics-page',
   templateUrl: './statistics-page.component.html',
-  styleUrls: ['./statistics-page.component.css']
+  styleUrls: ['./statistics-page.component.css'],
 })
-export class StatisticsPageComponent {
+export class StatisticsPageComponent implements OnInit {
+  projects!: Project[];
+  projectId!: string;
+  project: Project;
+  url = 'http://localhost:4040/v1/api/projects';
 
+  constructor(
+    private rest: RestApiService,
+    private data: DataService,
+    private toasrt: ToastrService
+  ) {
+    this.project = new Project();
+  }
+
+  ngOnInit() {
+    this.rest
+      .get(this.url)
+      .then((data) => {
+        this.projects = (data as { data: Project[] }).data;
+      })
+      .catch((error) => {
+        this.data.error(error['message']);
+        this.toasrt.error(error['message'], 'Error!');
+      });
+  }
+
+  handleChangeProject() {
+    this.rest
+      .getOne(this.url, this.projectId)
+      .then((data) => {
+        this.project = (data as { data: Project }).data;
+      })
+      .catch((error) => {
+        this.data.error(error['message']);
+        this.toasrt.error(error['message'], 'Error!');
+      });
+  }
 }
